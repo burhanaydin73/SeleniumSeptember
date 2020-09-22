2342f3679550e7773b6f31e32b7a950ca470cb50
Burhan Aydin <eaglewing126@gmail.com>
2020-09-21 22:18:22 -0500

    first commit

---
+++ src/com/class1/Class1.java
@@ -0,0 +1,25 @@
+package com.class1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Class1 {
+
+	public static void main(String[] args) throws InterruptedException{
+	
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	 
+		
+		driver.get("https://www.monster.com/");
+		Thread.sleep(3000);
+		driver.findElement(By.id("q2")).sendKeys("selenium");
+		
+		//driver.findElement(By.id("email")).sendKeys("eaglewing126@gmail.com");
+	
+		
+		//driver.findElement(By.name("login")).click();
+		driver.quit();
+}
+}
